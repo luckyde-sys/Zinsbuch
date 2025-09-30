83e1bcad20187d0de96f6cf87addee6a448f2ee2
singa162 <arjun.singh.as1@businesspartner.roche.com>
2025-09-30 20:41:19 +0200

    Integration with forms and frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BorrowerList />
+  </div>
+</template>
+
+<script>
+import BorrowerList from './components/BorrowerList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BorrowerList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
